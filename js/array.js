6118b2fecd729a605cbe5cef0ca06f267a067cc8
luis <s.r.l.a78637495@gmail.com>
2017-04-14 01:32:06 -0400

    borrar

--- js/array.js
+++ js/array.js
@@ -6,3 +6,12 @@ Array.prototype.buscar = function(element){
 		}
 	}
 }
+Array.prototype.borrar = function(element){
+	for (var i = 0; i < this.length; i++) {
+		if(this[i] === element )
+		{
+			alert("Se elimino el nombre:"+this[i]);
+			this.splice(i,1);
+		}
+	}
+}
